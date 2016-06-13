2bc58773c5c46c21a05975c06dc1ac9f0a407c0f
Asparuh Krastev <a.krastev@up2.nl>
2016-06-13 23:23:23 +0300

    Homework OOP

--- HomeworkOOP/Building/main.cpp
+++ HomeworkOOP/Building/main.cpp
@@ -6,11 +6,13 @@ using namespace std;
 
 class Company {
 public:
-    Company(string name, unsigned int officeCount, unsigned int employeeCount, unsigned int vacanciesCount);
-    ~Company();
-    inline unsigned int getOfficeCount() const
+    Company(const string& name, unsigned int employeeCount, unsigned int vacanciesCount)
+        :m_name(name),m_employeeCount(employeeCount), m_vacanciesCount(vacanciesCount)
+    {}
+
+    inline string getName() const
     {
-        return m_officeCount;
+        return m_name;
     }
     inline unsigned int getEmployeeCount() const
     {
@@ -27,106 +29,82 @@ public:
 
 private:
     string m_name;
-    unsigned int m_officeCount;
     unsigned int m_employeeCount;
     unsigned int m_vacanciesCount;
 };
 
-Company::Company(string name, unsigned int officeCount=0, unsigned int employeeCount=0, unsigned int vacanciesCount=0)
-{
-    m_name = name;
-    m_officeCount = officeCount;
-    m_employeeCount = employeeCount;
-    m_vacanciesCount = vacanciesCount;
-}
 
-Company::~Company()
-{
-}
+class Floor {
+public:
+    Floor(unsigned int officeCount=0)
+        :m_officeCount(officeCount)
+    {}
 
+    unsigned int getOfficeCount() const {
+        return m_officeCount;
+    }
+
+private:
+    unsigned int m_officeCount;
+};
 
 
 class Building {
 public:
-    Building(string name, unsigned int floorsNumber, vector<Company> companies);
+    Building(const Company& company, const vector<Floor>& floors)
+        :m_company(company),m_floors(floors)
+    {}
     ~Building();
 
-    inline string getName() const
-    {
-        return m_name;
-    }
-    unsigned int getEmployeeCount() const;
-    unsigned int getVacanciesCount() const;
-    unsigned int getSeatCount() const;
     unsigned int getOfficeCount() const;
 
+    inline string getCompanyName() const
+    {
+        return m_company.getName();
+    }
+    inline unsigned int getEmployeeCount() const
+    {
+        return m_company.getEmployeeCount();
+    }
+    inline unsigned int getVacanciesCount() const
+    {
+        return m_company.getVacanciesCount();
+    }
+    inline unsigned int getSeatCount() const
+    {
+        return m_company.getSeatCount();
+    }
     inline float getEmployeesVsSeats() const
     {
-        return getEmployeeCount() / getSeatCount();
+        return (float)getEmployeeCount() / getSeatCount();
     }
     inline float getEmployeesPerFloor() const
     {
-        return getEmployeeCount() / m_floorsNumber;
+        return (float)getEmployeeCount() / m_floors.size();
     }
     inline float getOfficesPerFloor() const
     {
-        return getOfficeCount() / m_floorsNumber;
+        return (float)getOfficeCount() / m_floors.size();
     }
     inline float getEmployeesPerOffice() const
     {
-        return getEmployeeCount() / getOfficeCount();
+        return (float)getEmployeeCount() / getOfficeCount();
     }
 
 private:
-    string m_name;
-    unsigned int m_floorsNumber;
-    vector<Company> m_companies;
-
+    Company m_company;
+    vector<Floor> m_floors;
 };
-
-Building::Building(string name, unsigned int floorsNumber, vector<Company> companies)
-{
-    m_name = name;
-    m_floorsNumber = floorsNumber;
-    m_companies = companies;
-}
 
 Building::~Building()
 {
 }
 
-unsigned int Building::getEmployeeCount() const
-{
-    unsigned int count = 0;
-    for(size_t i=0; i<m_companies.size(); i++) {
-        count += m_companies[i].getEmployeeCount();
-    }
-    return count;
-}
-
-unsigned int Building::getVacanciesCount() const
-{
-    unsigned int count = 0;
-    for(size_t i=0; i<m_companies.size(); i++) {
-        count += m_companies[i].getVacanciesCount();
-    }
-    return count;
-}
-
-unsigned int Building::getSeatCount() const
-{
-    unsigned int count = 0;
-    for(size_t i=0; i<m_companies.size(); i++) {
-        count += m_companies[i].getSeatCount();
-    }
-    return count;
-}
-
 unsigned int Building::getOfficeCount() const
 {
     unsigned int count = 0;
-    for(size_t i=0; i<m_companies.size(); i++) {
-        count += m_companies[i].getOfficeCount();
+    for(size_t i=0; i<m_floors.size(); i++) {
+        count += m_floors[i].getOfficeCount();
     }
     return count;
 }
@@ -135,32 +113,64 @@ bool EmployeeCmp(Building a,Building b) { return a.getEmployeeCount()<b.getEmplo
 
 bool VacanciesCmp(Building a,Building b) { return a.getVacanciesCount()<b.getVacanciesCount(); }
 
-bool HighestEmployessVsSeatsCmp(Building a,Building b) { return a.getEmployeesVsSeats()<b.getEmployeesVsSeats(); }
+bool EmployeesVsSeatsCmp(Building a,Building b) { return a.getEmployeesVsSeats()<b.getEmployeesVsSeats(); }
 
-bool MostEmployeePerFloorCmp(Building a,Building b) { return a.getEmployeesPerFloor()<b.getEmployeesPerFloor(); }
+bool EmployeePerFloorCmp(Building a,Building b) { return a.getEmployeesPerFloor()<b.getEmployeesPerFloor(); }
 
-bool MostOfficesPerFloorCmp(Building a,Building b) { return a.getOfficesPerFloor()<b.getOfficesPerFloor(); }
+bool OfficesPerFloorCmp(Building a,Building b) { return a.getOfficesPerFloor()<b.getOfficesPerFloor(); }
 
-bool MostEmployeePerOfficeCmp(Building a,Building b) { return a.getEmployeesPerOffice()<b.getEmployeesPerOffice(); }
+bool EmployeePerOfficeCmp(Building a,Building b) { return a.getEmployeesPerOffice()<b.getEmployeesPerOffice(); }
 
 
 int main()
 {
     vector<Building> businessPark = {
-        Building("XYZ industries", 6, {Company("XYZ industries", 127, 600, 196)}),
-        Building("Rapid development crew", 8, {Company("Rapid development crew", 210, 822, 85), Company("Restaurant")}),
-        Building("Softuni", 11, {Company("Softuni", 106, 200, 60)}),
+        Building(Company("XYZ industries", 600, 196), {
+            // 6 floors, 127 offices
+            Floor(21),
+            Floor(21),
+            Floor(21),
+            Floor(21),
+            Floor(21),
+            Floor(22),
+        }),
+        Building(Company("Rapid development crew", 822, 85), {
+            // 8 floors, 210 offices and a restaurant
+            Floor(),
+            Floor(30),
+            Floor(30),
+            Floor(30),
+            Floor(30),
+            Floor(30),
+            Floor(30),
+            Floor(30),
+        }),
+        Building(Company("Softuni", 200, 60), {
+            // 11 floors, 106 offices
+            Floor(10),
+            Floor(10),
+            Floor(10),
+            Floor(10),
+            Floor(10),
+            Floor(10),
+            Floor(10),
+            Floor(9),
+            Floor(9),
+            Floor(9),
+            Floor(9),
+        }),
     };
 
-    cout << "Most employees: " << max_element(businessPark.begin(), businessPark.end(), EmployeeCmp)->getName() << endl;
-    cout << "Most free places: " << max_element(businessPark.begin(), businessPark.end(), VacanciesCmp)->getName() << endl;
-    cout << "Highest coefficient employees/(employees + free seats): " << max_element(businessPark.begin(), businessPark.end(), HighestEmployessVsSeatsCmp)->getName() << endl;
-    cout << "Most people per floor: " << max_element(businessPark.begin(), businessPark.end(), MostEmployeePerFloorCmp)->getName() << endl;
-    cout << "Least people per floor: " << min_element(businessPark.begin(), businessPark.end(), MostEmployeePerFloorCmp)->getName() << endl;
-    cout << "Most offices per floor: " << max_element(businessPark.begin(), businessPark.end(), MostOfficesPerFloorCmp)->getName() << endl;
-    cout << "Least offices per floor: " << min_element(businessPark.begin(), businessPark.end(), MostOfficesPerFloorCmp)->getName() << endl;
-    cout << "Most people per floor: " << max_element(businessPark.begin(), businessPark.end(), MostEmployeePerOfficeCmp)->getName() << endl;
-    cout << "Least people per floor: " << min_element(businessPark.begin(), businessPark.end(), MostEmployeePerOfficeCmp)->getName() << endl;
+    cout << "Most employees: building \"" << max_element(businessPark.begin(), businessPark.end(), EmployeeCmp)->getCompanyName() << "\"" << endl;
+    cout << "Most free places: building \"" << max_element(businessPark.begin(), businessPark.end(), VacanciesCmp)->getCompanyName() << "\"" << endl;
+    cout << "Highest coefficient employees/(employees + free seats): building \"" << max_element(businessPark.begin(), businessPark.end(), EmployeesVsSeatsCmp)->getCompanyName() << "\"" << endl;
+    cout << "Most people per floor: building \"" << max_element(businessPark.begin(), businessPark.end(), EmployeePerFloorCmp)->getCompanyName() << "\"" << endl;
+    cout << "Least people per floor: building \"" << min_element(businessPark.begin(), businessPark.end(), EmployeePerFloorCmp)->getCompanyName() << "\"" << endl;
+    cout << "Most offices per floor: building \"" << max_element(businessPark.begin(), businessPark.end(), OfficesPerFloorCmp)->getCompanyName() << "\"" << endl;
+    cout << "Least offices per floor: building \"" << min_element(businessPark.begin(), businessPark.end(), OfficesPerFloorCmp)->getCompanyName() << "\"" << endl;
+    cout << "Most people per office: building \"" << max_element(businessPark.begin(), businessPark.end(), EmployeePerOfficeCmp)->getCompanyName() << "\"" << endl;
+    cout << "Least people per office: building \"" << min_element(businessPark.begin(), businessPark.end(), EmployeePerOfficeCmp)->getCompanyName() << "\"" << endl;
+
     return 0;
 }
 
